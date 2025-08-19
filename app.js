658c2e31b5133c1060b2745fde6ff9d2885aec65
AustralianMint <thomas.frey@code.berlin>
2025-08-19 17:31:53 +0200

    add: project files

---
+++ app.js
@@ -0,0 +1,72 @@
+const bells = new Audio('./achievementBell.wav');
+const startBtn = document.querySelector('.btn-start');
+const resetBtn = document.querySelector('.btn-reset');
+const pauseBtn = document.querySelector('.btn-pause');
+const minuteDiv = document.querySelector('.minutes');
+const secondDiv = document.querySelector('.seconds');
+
+let myInterval;
+let state = true;
+let totalSeconds;
+
+const appTimer = () => {
+
+  //Turns displayed time string into Int.
+  const sessionAmount = Number.parseInt(minuteDiv.textContent);
+  if (state) {
+    state = false;
+
+    if (totalSeconds === undefined) {
+      totalSeconds = sessionAmount * 60;
+    }
+
+    const updateSeconds = () => {
+      totalSeconds--;
+
+      let minutesLeft = Math.floor(totalSeconds/60);
+      let secondsLeft = totalSeconds % 60;
+
+      if(secondsLeft < 10) {
+        secondDiv.textContent = '0' + secondsLeft;
+      } else {
+        secondDiv.textContent = secondsLeft;
+      }
+
+      minuteDiv.textContent = minutesLeft;
+
+      if(minutesLeft === 0 && secondsLeft === 0) {
+        bells.play();
+        clearInterval(myInterval);
+        state = true;
+        totalSeconds = undefined;
+      }
+    }
+
+    myInterval = setInterval(updateSeconds, 1000);
+
+  } else {
+    alert('Session has already been started.');
+  }
+}
+
+const resetTimer = () => {
+  state = true;
+  clearInterval(myInterval);
+
+  minuteDiv.textContent = "25";
+  secondDiv.textContent = "00";
+
+  console.log("Clear was pressed!");
+}
+
+const pauseTimer = () => {
+  if (!state) {
+    state = true;
+    clearInterval(myInterval);
+    console.log("Pause was pressed!");
+  }
+}
+
+startBtn.addEventListener("click", appTimer);
+resetBtn.addEventListener("click", resetTimer);
+pauseBtn.addEventListener("click", pauseTimer);
