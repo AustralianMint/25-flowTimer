168c885b5a34267ecd1252fe1318fd2ed0bfed20
AustralianMint <thomas.frey@code.berlin>
2025-08-20 14:54:19 +0200

    fix: logic for correct time after reset

--- app.js
+++ app.js
@@ -7,25 +7,30 @@ const secondDiv = document.querySelector('.seconds');
 
 let myInterval;
 let state = true;
+let pauseState = false;
 let totalSeconds;
 
 const appTimer = () => {
 
-  //Turns displayed time string into Int.
+  //Turns displayed time string into Int, result = 25
   const sessionAmount = Number.parseInt(minuteDiv.textContent);
+
   if (state) {
     state = false;
 
+    //Executes when clock has never been started
     if (totalSeconds === undefined) {
       totalSeconds = sessionAmount * 60;
     }
 
+    //Logic for changing the displaid time.
     const updateSeconds = () => {
       totalSeconds--;
 
       let minutesLeft = Math.floor(totalSeconds/60);
       let secondsLeft = totalSeconds % 60;
 
+      //Displays changes displaid time to time remaining
       if(secondsLeft < 10) {
         secondDiv.textContent = '0' + secondsLeft;
       } else {
@@ -52,6 +57,7 @@ const appTimer = () => {
 const resetTimer = () => {
   state = true;
   clearInterval(myInterval);
+  totalSeconds = undefined;
 
   minuteDiv.textContent = "25";
   secondDiv.textContent = "00";
@@ -63,6 +69,7 @@ const pauseTimer = () => {
   if (!state) {
     state = true;
     clearInterval(myInterval);
+
     console.log("Pause was pressed!");
   }
 }
